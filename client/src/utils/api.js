0ea7863a62926e2ebe62f16874a5388a38d2ec71
Harsh Patel <harshdhansinghpatel@gmail.com>
2025-10-25 23:52:24 +0530

    Updated API baseURL for deployed backend

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
+// const api = axios.create({
+//   baseURL: "http://localhost:8000", // ✅ backend port
+// });
 const api = axios.create({
-  baseURL: "http://localhost:8000", // ✅ backend port
+  baseURL: "https://oneorgassignment.vercel.app", // ✅ your deployed backend URL
 });
 
 export default api;
