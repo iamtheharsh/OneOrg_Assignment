6eee48638699c17087287fd8fbc1418ca9fbb324
Harsh Patel <harshdhansinghpatel@gmail.com>
2025-10-25 20:25:16 +0530

    Changes for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,14 @@
+// import axios from "axios";
+
+// const api = axios.create({
+//   baseURL: "http://localhost:8000", // ✅ backend port
+// });
+
+// export default api;
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000", // ✅ backend port
+  baseURL: "https://oneorgassignment-kgx8depao-iamtheharshs-projects.vercel.app/",
 });
 
 export default api;
